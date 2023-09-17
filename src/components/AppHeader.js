f4db2cb1ef7ea3860dd65f3fedcd32da101d7d64
LunhA <luugiatinh34@gmail.com>
2023-09-17 22:01:24 +0700

    demo header

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,6 +1,18 @@
 function AppHeader() {
   return (
-    "test"
+    <div className= "header-container">
+      <div className="logo">PDB</div>
+      <div className="searchbar-container">
+        <input className="searchbar" placeholder="Famous people, characters, movies, anime,..."></input>
+      </div>
+      <div className="profile-container">
+        {/* <img className="profile--img" scr=""> */}
+          <div className="profile--img-name">
+              <p>Lunh</p>
+          </div>
+        {/* </img>  */}
+      </div>
+    </div>
   );
 }
 
