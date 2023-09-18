ac5972fdd651acd626f559b28b24be605507d3f9
LunhA <luugiatinh34@gmail.com>
2023-09-18 19:17:34 +0700

    add header, install bootstrap

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,18 +1,19 @@
 function AppHeader() {
   return (
-    <div className= "header-container">
-      <div className="logo">PDB</div>
-      <div className="searchbar-container">
-        <input className="searchbar" placeholder="Famous people, characters, movies, anime,..."></input>
-      </div>
-      <div className="profile-container">
-        {/* <img className="profile--img" scr=""> */}
-          <div className="profile--img-name">
-              <p>Lunh</p>
-          </div>
-        {/* </img>  */}
-      </div>
-    </div>
+    // <div className= "header-container">
+    //   <div className="logo">PDB</div>
+    //   <div className="searchbar-container">
+    //     <input className="searchbar" placeholder="Famous people, characters, movies, anime,..."></input>
+    //   </div>
+    //   <div className="profile-container">
+    //     {/* <img className="profile--img" scr=""> */}
+    //       <div className="profile--img-name">
+    //           <p>Lunh</p>
+    //       </div>
+    //     {/* </img>  */}
+    //   </div>
+    // </div>
+    <div>"Test"</div>
   );
 }
 
