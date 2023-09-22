fd3c152a57d046141d5f7a06539320b67f496c98
LunhA <luugiatinh34@gmail.com>
2023-09-22 21:52:56 +0700

    22/09/23

--- src/views/Landing.js
+++ src/views/Landing.js
@@ -2,8 +2,32 @@
 
 function Landing(){
     return(
-        <div className="container">
-            
+        <div className="main-page">
+            <div className="main-container">
+                <div className="main-option-column">
+                    TOP AIRING
+                </div>
+                <div className="main-content-container">
+                    <div className="promos-character-banner">
+                        <a href="#" className="promos-character-banner--a">
+                            <div className="promos-character-img">
+                                <img src="https://static1.personality-database.com/profile_images/67550e7927914846bb6775ef731236c2.png" className="promos-character-img--img"></img>
+                            </div>
+                            <div className="promos-character-info" >
+                                <div className="promos-character-name">NAMI</div>
+                                <div className="promos-character-movie">One Piece 2023</div>
+                                <div className="promos-character-personality">ENTJ 3w2 sp/so 369 votes</div>
+                            </div>
+                        </a>
+                    </div>
+                    <div className="category-container">
+                        <div className="field-container">
+                            <div className="field-name">Ten Linh Vuc </div>
+                            <div className="field-items"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
