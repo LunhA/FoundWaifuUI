dbb3f42334522bd5e58eccbc189e8c56c725f2a8
LunhA <luugiatinh34@gmail.com>
2023-10-10 22:08:56 +0700

    micro changes

--- src/views/QuestionPage.js
+++ src/views/QuestionPage.js
@@ -1,64 +1,108 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-const items = [
-  "Mục 1",
-  "Mục 2",
-  "Mục 3",
-  "Mục 4",
-  "Mục 5",
-  "Mục 6",
-  "Tôi Không Rõ",
+const introvertedItems = [
+  "ISTJ",
+  "ISFJ",
+  "INFJ",
+  "INTJ",
+  "ISTP",
+  "ISFP",
+  "INFP",
+  "INTP",
+];
+
+const extravertedItems = [
+  "ESTP",
+  "ESFP",
+  "ENFP",
+  "ENTP",
+  "ESTJ",
+  "ESFJ",
+  "ENFJ",
+  "ENTJ",
 ];
 
 function QuestionPage() {
   const history = useNavigate();
   const [selectedItems, setSelectedItems] = useState([]);
+  const [canProceed, setCanProceed] = useState(true);
 
   const handleItemSelection = (item) => {
-    if (selectedItems.length < 4 || item === "Tôi Không Rõ") {
-      setSelectedItems([...selectedItems, item]);
-    }
-  };
+    const isItemSelected = selectedItems.includes(item);
 
-  const handleNextButtonClick = () => {
-    if (selectedItems.length === 4 && !selectedItems.includes("Tôi Không Rõ")) {
-      history("/home"); // Điều hướng đến trang home nếu điều kiện thỏa mãn
-    } else {
+    if (selectedItems.length < 4 || isItemSelected) {
+      if (isItemSelected) {
+        setSelectedItems(selectedItems.filter((selected) => selected !== item));
+      } else {
+        setSelectedItems([...selectedItems, item]);
+      }
+
+      // Kiểm tra nếu đã chọn "Tôi Không Rõ" thì vô hiệu hóa nút "Tiếp Theo"
       if (selectedItems.includes("Tôi Không Rõ")) {
-        history("/input"); //
+        setCanProceed(false);
+      } else {
+        setCanProceed(true);
       }
     }
   };
 
+  const handleNextButtonClick = () => {
+    if (selectedItems.length === 4) {
+      history("/home");
+    } else if (selectedItems.includes("Tôi Không Rõ")) {
+      history("/input");
+    } else {
+      alert("Vui lòng chọn đủ 4 nhóm tính cách");
+    }
+  };
   return (
     <div className="question-page">
       <div className="question-content">
         <h2>Câu hỏi thứ 1: Chọn 4 mục mà bạn quan trọng nhất:</h2>
-        <ul>
-          {items.map((item, index) => (
-            <li key={index}>
-              <label className="custom-checkbox">
-                <input
-                  type="checkbox"
-                  value={item}
-                  onChange={() => handleItemSelection(item)}
-                  disabled={selectedItems.length === 4}
-                />
-                <span className="checkmark"></span>
-              </label>
-                {item}
-            </li>
-          ))}
-        </ul>
+        <div className="columns">
+          <div className="column">
+            <ul>
+              {introvertedItems.map((item, index) => (
+                <li key={index}>
+                  <label className="custom-checkbox">
+                    <input
+                      type="checkbox"
+                      value={item}
+                      onChange={() => handleItemSelection(item)}
+                      disabled={selectedItems.length === 4 && !selectedItems.includes(item)}
+                    />
+                    <span className="checkmark"></span>
+                  </label>
+                  {item}
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="column">
+            <ul>
+              {extravertedItems.map((item, index) => (
+                <li key={index}>
+                  <label className="custom-checkbox">
+                    <input
+                      type="checkbox"
+                      value={item}
+                      onChange={() => handleItemSelection(item)}
+                      disabled={selectedItems.length === 4 && !selectedItems.includes(item)}
+                    />
+                    <span className="checkmark"></span>
+                  {item}
+                  </label>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
 
         <button
           className="btn-info"
           onClick={handleNextButtonClick}
-          disabled={
-            selectedItems.length !== 4 &&
-            !selectedItems.includes("Tôi Không Rõ")
-          }
+          // disabled={!canProceed}
         >
           Tiếp Theo
         </button>
@@ -66,4 +110,5 @@ function QuestionPage() {
     </div>
   );
 }
+
 export default QuestionPage;
