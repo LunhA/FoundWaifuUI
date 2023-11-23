7e3fa965aea8bf43a5ee167b12560f4b71382691
LunhA <luugiatinh34@gmail.com>
2023-11-23 20:31:31 +0700

    add profile UI

---
+++ src/api/recommendation.api.js
@@ -0,0 +1,109 @@
+import { useState, useEffect } from "react";
+
+
+const recommendation = {
+ getRecommendedChars(selectedTypes) {
+   return [{
+    id: 1,
+    name: "Alice",
+    image: "",
+    type: "INTJ",
+    anime: "Shinigami Bocchan to Kuro Maid",
+   },{
+    id: 2,
+    name: "Alice",
+    image: "",
+    type: "INTJ",
+    anime: "Shinigami Bocchan to Kuro Maid",
+   },{
+    id: 3,
+    name: "Alice",
+    image: "",
+    type: "INTJ",
+    anime: "Shinigami Bocchan to Kuro Maid",
+   },{
+    id: 4,
+    name: "Alice",
+    image: "",
+    type: "INTJ",
+    anime: "Shinigami Bocchan to Kuro Maid",
+   },{
+    id: 5,
+    name: "Alice",
+    image: "",
+    type: "INTJ",
+    anime: "Shinigami Bocchan to Kuro Maid",
+   },]     
+},
+
+ getSearchChars(UserSearch) {
+    return [{
+        id: 1,
+        name: "Alice",
+        image: "",
+        type: "INTJ",
+        anime: "Shinigami Bocchan to Kuro Maid",
+       },{
+        id: 2,
+        name: "Alice",
+        image: "",
+        type: "INTJ",
+        anime: "Shinigami Bocchan to Kuro Maid",
+       },]
+},
+
+ getCharacterDetail(id) {
+    return [{
+        id: 1,
+        name: "Alice",
+        image: "",
+        type: "INTJ",
+        anime: "Shinigami Bocchan to Kuro Maid",
+    }]
+},
+
+ getRelatedChars(id){
+    return [{
+        id: 1,
+        name: "Alice",
+        image: "",
+        type: "INTJ",
+        anime: "Shinigami Bocchan to Kuro Maid",
+    }]
+},
+
+ getLikedChars(userid){
+    return  [{
+        id: 1,
+        name: "Alice",
+        image: "",
+        type: "INTJ",
+        anime: "Shinigami Bocchan to Kuro Maid",
+       },{
+        id: 2,
+        name: "Alice",
+        image: "",
+        type: "INTJ",
+        anime: "Shinigami Bocchan to Kuro Maid",
+       },{
+        id: 3,
+        name: "Alice",
+        image: "",
+        type: "INTJ",
+        anime: "Shinigami Bocchan to Kuro Maid",
+       },{
+        id: 4,
+        name: "Alice",
+        image: "",
+        type: "INTJ",
+        anime: "Shinigami Bocchan to Kuro Maid",
+       },{
+        id: 5,
+        name: "Alice",
+        image: "",
+        type: "INTJ",
+        anime: "Shinigami Bocchan to Kuro Maid",
+       },]
+},
+}
+
